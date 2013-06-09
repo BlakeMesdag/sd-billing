e674f0dcab20df5785e8b4cb2bb1a07a23734bd6
Blake Mesdag <blakemesdag@gmail.com>
2013-06-09 15:17:43 -0400

    Setup for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,4 @@ SdBilling::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'somethingdevio.us',
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
 end
