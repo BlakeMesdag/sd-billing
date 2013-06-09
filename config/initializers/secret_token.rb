8235bb7002dec1cd4798906e2c0bb9b3ee7269b2
Blake Mesdag <blakemesdag@gmail.com>
2013-06-08 22:51:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SdBilling::Application.config.secret_key_base = '35aa70aa40036aa919e1c239776b24bea1e08e390652a11a2342318cfe15b830d39a96e5fb409c9b09402ca35f9164fc4d8cc5ea5183051af707af3d97cce57a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SdBilling::Application.config.session_store :cookie_store, key: '_sd-billing_session'
